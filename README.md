185d0407914844db0f908f73a01f780bd82d43fa
lawrence <aceliuchanghong@gmail.com>
2023-12-01 21:07:20 +0800

    push,之后改,好冷啊

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ```git
 git rm -r --cached .idea
 ```
+learn from:
+
+https://levelup.gitconnected.com/building-a-website-starter-with-fastapi-92d077092864
---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+import uvicorn
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from library.helpers import *
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = openfile("home.md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def show_page(request: Request, page_name: str):
+    data = openfile(page_name + ".md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
