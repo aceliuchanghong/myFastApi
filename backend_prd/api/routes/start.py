cb9104cf2e6ccd05fdc7c4426bcaf3d3af4771e3
Lawrence-AIP <liuch@allinfinance.com>
2024-01-31 15:02:00 +0800

    push

--- backend_prd/api/routes/start.py
+++ backend_prd/api/routes/start.py
@@ -1,12 +1,6 @@
-import re
 from fastapi import Request
-
+from config import templates
 from fastapi import APIRouter
-from langchain_community.document_loaders import WebBaseLoader
-
-from backend_prd.api.models.others import InputInfo
-from config import templates, proxies
-from utils.url_valid import is_valid_url, url_regex
 
 router = APIRouter()
 
@@ -21,40 +15,26 @@ def get_page_contact():
     return {"message": "Get image list"}
 
 
-@router.get("/Pages/Login")
-def get_page_login(request: Request):
+@router.get("/Pages/Crawl")
+def get_page_crawl(request: Request):
+    return templates.TemplateResponse("test_files.html", {"request": request})
+
+
+@router.get("/Pages/Image")
+def get_image_deal(request: Request):
+    return templates.TemplateResponse("test_files.html", {"request": request})
+
+
+@router.get("/Pages/Video")
+def get_video_deal(request: Request):
+    return templates.TemplateResponse("test_files.html", {"request": request})
+
+
+@router.get("/Pages/Audio")
+def get_audio_deal(request: Request):
     return templates.TemplateResponse("test_files.html", {"request": request})
 
 
 @router.get("/Docs")
 def get_docs_list(request: Request):
     return templates.TemplateResponse("docs/index.html", {"request": request})
-
-
-@router.post("/webpage/info")
-async def process_info(request: Request, input_data: InputInfo):
-    """
-    eg:
-        https://www.4hu.tv/view/202401/79272.html
-        https://www.sohu.com/a/744144846_121687421
-        https://www.runoob.com/fastapi/fastapi-form.html
-    :param request:
-    :param input_data:
-    :return:
-    """
-    modified_text = input_data.inputText
-    # 可以根据checkboxState的值来调整返回的内容
-    my_proxy = None
-    if input_data.checkboxState:
-        my_proxy = proxies
-    if modified_text == "因为困难多壮志":
-        return {"output_text": modified_text + ",不教红尘惑坚心"}
-    elif is_valid_url(modified_text.strip()) and re.match(url_regex, modified_text.strip()):
-        loader = WebBaseLoader(modified_text, proxies=my_proxy)
-        docs = loader.load()
-        content = ""
-        for doc in docs:
-            content += doc.page_content.replace("\n", "").replace(" ", "")
-        return {"output_text": content}
-    else:
-        return {"output_text": "不规则链接"}
--- backend_prd/api/routes/webpage.py
+++ backend_prd/api/routes/webpage.py
@@ -1,28 +1,38 @@
 from fastapi import APIRouter
+from langchain_community.document_loaders import WebBaseLoader
+import re
+from backend_prd.api.models.others import InputInfo
+from fastapi import Request
+from config import proxies
+from utils.url_valid import is_valid_url, url_regex
 
 router = APIRouter()
 
 
-@router.get("/")
-def get_page_list():
-    return {"message": "Get page list"}
-
-
-@router.post("/")
-def upload_page():
-    return {"message": "Upload page"}
-
-
-@router.get("/{page_id}")
-def get_page(page_id: int):
-    return {"message": f"Get page {page_id}"}
-
-
-@router.put("/{page_id}")
-def update_page(page_id: int):
-    return {"message": f"Update page {page_id}"}
-
-
-@router.delete("/{page_id}")
-def delete_page(page_id: int):
-    return {"message": f"Delete page {page_id}"}
+@router.post("/info")
+async def process_info(request: Request, input_data: InputInfo):
+    """
+    eg:
+        https://www.4hu.tv/view/202401/79272.html
+        https://www.sohu.com/a/744144846_121687421
+        https://www.runoob.com/fastapi/fastapi-form.html
+    :param request:
+    :param input_data:
+    :return:
+    """
+    modified_text = input_data.inputText
+    # 可以根据checkboxState的值来调整返回的内容
+    my_proxy = None
+    if input_data.checkboxState:
+        my_proxy = proxies
+    if modified_text == "因为困难多壮志":
+        return {"output_text": modified_text + ",不教红尘惑坚心"}
+    elif is_valid_url(modified_text.strip()) and re.match(url_regex, modified_text.strip()):
+        loader = WebBaseLoader(modified_text, proxies=my_proxy)
+        docs = loader.load()
+        content = ""
+        for doc in docs:
+            content += doc.page_content.replace("\n", "").replace(" ", "")
+        return {"output_text": content}
+    else:
+        return {"output_text": "不规则链接"}
